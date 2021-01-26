1666465ed552308613bed1f43574caa3f96bc5e2
Jiraphun Jitrukro <jiraphun.j@clicknext.com>
2021-01-26 23:56:20 +0700

    finish

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,89 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ex1
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+    <button class="greeting-button" @click="onCLick">Good Morning</button>
+    <button class="greeting-button" @click="onCLick">Good Afternoon</button>
+    <button class="greeting-button" @click="onCLick">Good Night</button>
+    <button class="dont-click-me">Don’t Click Me</button>
+    <div
+      class="question-section"
+      v-for="(item, index) in renderQuestion"
+      :key="index"
+    >
+      <b-form-group :label="item.question" v-slot="{ ariaDescribedby }">
+        <span
+          class="text-danger"
+          v-if="
+            !checkAnswer(item.selected, item.answers, item.correctAnswer) &&
+              item.selected
+          "
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+          Correct answer is ({{ item.correctAnswer }})
+        </span>
+        <b-form-radio-group
+          v-model="item.selected"
+          :options="item.answers"
+          :aria-describedby="ariaDescribedby"
+          stacked
+        ></b-form-radio-group>
+      </b-form-group>
     </div>
+    <button @click="checkUserAnswerAllQuestion">
+      checkUserAnswerAllQuestion
+    </button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      myQuestions: [
+        {
+          question: "1 + 2 is ?",
+          answers: ["1", "2", "3"],
+          correctAnswer: 2,
+          selected: undefined
+        },
+        {
+          question: "What is the best site for Web Programmer ?",
+          answers: ["Stack Overflow", "Quora", "w3school"],
+          correctAnswer: 0,
+          selected: undefined
+        },
+        {
+          question: "Who is Prime minister fo Thailand ?",
+          answers: [
+            "Prayut Chan-o-cha",
+            "Yingluck Shinawatra",
+            "Abhisit Vejjajiva",
+            "Somchai Wongsawat"
+          ],
+          correctAnswer: 0,
+          selected: undefined
+        }
+      ]
+    };
+  },
+  computed: {
+    renderQuestion() {
+      return this.myQuestions;
+    }
+  },
+  methods: {
+    onCLick() {
+      alert("Hint : $(‘.greeting-button’)");
+    },
+    checkAnswer(selected, data, correctAnswer) {
+      return correctAnswer === data.indexOf(selected);
+    },
+    checkUserAnswerAllQuestion() {
+      this.myQuestions.forEach((element, index) => {
+        if (!element.selected)
+          alert(`You have not answered question ${index + 1} `);
+      });
+    }
+  }
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
